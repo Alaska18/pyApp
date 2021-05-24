4d122b3f37af8bac7b1a06309d3350b80b31f4e6
Afshan Ahmed <afshanahmed.ec18@rvce.edu.in>
2021-05-24 14:11:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from slack_bolt import App
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+
+@app.command("/hi")
+def repeat_text(ack, say, command):
+    ack()
+    say(f"Hey!")
+
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
